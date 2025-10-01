78c5dacc698c28f7905adbdf07565e139179b072
rafael <rafael_contato.exe@outlook.com>
2025-10-01 19:58:47 -0300

    contato configurado - operando

--- src/components/HeroFlat.jsx
+++ src/components/HeroFlat.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function heroFlat(props) {
-    console.log(props.image);
     return (
         <section className="localHero">
             <div className="imageLocalHero">
--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,10 +1,36 @@
 import React from "react";
+import { useState } from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import HeroFlat from "../../components/HeroFlat";
 
+import emailjs from "@emailjs/browser";
+
 export default function Contact() {
-    // const valueLocalText = "Entre em contato conosco";
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    function sendEmail(e) {
+        e.preventDefault();
+
+        const templateForm = {
+            from_name: name,
+            message: message,
+            email: email
+        }
+        emailjs.send("service_rk8cp3e", "template_xxd87mt", templateForm, "KWvqTRGscxKBMyo5S")
+        .then((response) => {
+            console.log("Email Enviado", response.status, response.text);
+        }, (erro) => {
+            console.log("Erro", erro)
+        })
+
+
+
+    }
+
     return (
         <>
             <Header />
@@ -26,22 +52,26 @@ export default function Contact() {
                         </div>
                     </div>
                     <div className="localFormRight sideForm">
-                        <form action="#" method="post">
+                        <form onSubmit={sendEmail}>
                             <label>
                                 Nome: <br />
-                                <input type="text" name="name" required placeholder="Digite seu nome" />
+                                <input type="text" onChange={(e) => setName(e.target.value)} required placeholder="Digite seu nome" />
                             </label>
 
                             <label>
                                 E-mail: <br />
-                                <input type="email" name="email" required placeholder="Digite seu melhor E-mail" />
+                                <input type="email" onChange={(e) => setEmail(e.target.value)} required placeholder="Digite seu melhor E-mail" />
                             </label>
                             <label> Digite sua mensagem: <br />
-                                <textarea name="textMesage" id="textMesage" placeholder="Digite sua mensagem"></textarea>
+                                <textarea onChange={(e) => setMessage(e.target.value)} placeholder="Digite sua mensagem"></textarea>
                             </label>
                             <div className="actBtn">
                                 <button type="submit">Enviar</button>
-                                <button type="reset">Limpar</button>
+                                <button type="button" onClick={() => {
+                                    setName("");
+                                    setEmail("");
+                                    setMessage("");
+                                }}>Limpar</button>
                             </div>
                         </form>
                     </div>
